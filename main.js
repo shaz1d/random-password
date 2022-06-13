90f07b640fd192b192838ad1f0b5f657e95fef1a
Shazidul Islam <shazidulislam8443@gmail.com>
2022-06-13 10:53:02 +0600

    added all javascript logics

--- main.js
+++ main.js
@@ -1,1 +1,113 @@
+const characters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "~",
+  "`",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "-",
+  "+",
+  "=",
+  "{",
+  "[",
+  "}",
+  "]",
+  ",",
+  "|",
+  ":",
+  ";",
+  "<",
+  ">",
+  ".",
+  "?",
+  "/",
+];
 
+const btn = document.getElementById("generate-pass-btn");
+const pass1Div = document.getElementById("pass-1");
+const pass2Div = document.getElementById("pass-2");
+
+const handleClick = () => {
+  pass1Div.textContent = generatePass();
+  pass2Div.textContent = generatePass();
+};
+btn.addEventListener("click", handleClick);
+
+const generatePass = () => {
+  let pass = "";
+  for (let i = 0; i < 15; i++) {
+    let randomIndex = Math.floor(Math.random() * characters.length);
+
+    pass += characters[randomIndex];
+  }
+  return pass;
+};
